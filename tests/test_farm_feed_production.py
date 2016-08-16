d0e3d19c73403f8d7e554529c5b4c619da8898ba
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:09 +0200

    Upgrade test

--- tests/test_farm_feed_production.py
+++ tests/test_farm_feed_production.py
@@ -4,29 +4,15 @@ import doctest
 import unittest
 
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     '''
     Test module.
     '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('farm_feed_production')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('farm_feed_production')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+    module = 'farm_feed_production'
 
 
 def suite():
