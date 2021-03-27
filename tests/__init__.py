9adb502aacbe607e41ac96c21bbdcd3b5f3cc43d
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-27 12:25:05 +0100

    Pyflakes fixes.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from .test_farm_feed_production import suite
+
+__all__ = ['suite']
